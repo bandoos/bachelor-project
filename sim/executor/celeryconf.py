083a0ffbd12d42b5725259eac2889995225a7566
CanAlbertCan <titesounCan@ALBERT.ARCH>
2020-06-09 18:13:12 +0200

    fix quotes in env vars

--- sim/executor/celeryconf.py
+++ sim/executor/celeryconf.py
@@ -15,11 +15,14 @@ import os
 
 env=os.environ
 
+broker_url=None
+
 if env.get('REDIS_URI') and env.get('REDIS_DB'):
     broker_url=env.get('REDIS_URI') + '/' + env.get('REDIS_DB')
 else:
     broker_url='redis://localhost:6379/2'
 
+result_backend=None
 
 if env.get('MONGODB_URI') and env.get('EXECUTOR_DB'):
     result_backend=env.get('MONGODB_URI') + '/' + env.get('EXECUTOR_DB')
