e1ba7a9ecd66ad6be4e3854861fabd3ca8bca71e
CanAlbertCan <titesounCan@ALBERT.ARCH>
2020-06-10 16:23:54 +0200

    commit documentation builds

---
+++ sim/executor/test/ctx.py
@@ -0,0 +1,39 @@
+# * combing context managers
+
+
+
+
+"""
+class A():
+    def __enter__(self):
+        print("enter A")
+        return self
+
+    def __exit__(self,*args):
+        print("exit A")
+
+class B(A):
+
+    def __call__(self):
+        raise Exception('ti')
+
+    def __enter__(self):
+        super(B,self).__enter__()
+        print("enter B")
+        return self
+
+    def __exit__(self,*args):
+        print("exit B")
+        return super(B,self).__exit__(*args)
+
+
+
+b = B()
+
+
+
+with b as ctx:
+##    ctx()
+    print("mona")
+
+"""
